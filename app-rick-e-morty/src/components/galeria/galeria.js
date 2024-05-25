0a127d39034e994d7b20b771e22ef5d162eed864
yurisavage <yuritec@gmail.com>
2024-05-25 20:12:28 -0300

    layout galeria

--- app-rick-e-morty/src/components/galeria/galeria.js
+++ app-rick-e-morty/src/components/galeria/galeria.js
@@ -20,18 +20,65 @@ export default function Galeria() {
             >
                 <CardMedia component='img' image="img/rick-morty3.gif" sx={{ width: 'auto', height: '27.5rem'}}>
                 </CardMedia>
-
-                <Box sx={{ mt: '1rem', width: '10rem', height: '10rem', borderRadius: '20rem', background: "#043c6e", }}>
-                    
+                
+                <div>
+                <Box sx={{ width: '10rem', height: '10rem', borderRadius: '20rem', background: "#043c6e",
+                            marginTop: '-2rem', ml: '0.5rem'
+                 }}>
+                    <Typography sx={{ color: '#ebe480', fontWeight: 'bold', fontSize: '2rem', 
+                                        mt: '3.5rem', ml: '2rem', paddingTop: '3.5rem'
+                                    }}
+                    >
+                        Morty
+                    </Typography>
                 </Box>
 
-                <Box sx={{ mt: '10rem', width: '10rem', height: '10rem', borderRadius: '20rem', background: "#043c6e", }}>
-                    
+                <Box sx={{ width: '10rem', height: '10rem', borderRadius: '20rem', background: "#043c6e", 
+                            marginTop: '4rem', ml: '0.5rem'
+                }}>
+                    <Typography sx={{ color: '#ebe480', fontWeight: 'bold', fontSize: '2rem', 
+                                        mt: '3.5rem', ml: '2.5rem', paddingTop: '3.5rem'
+                                    }}
+                    >
+                        Beth
+                    </Typography>
+                </Box>
+                </div>
+
+                <div>
+                <Box sx={{ mt: '10rem', width: '10rem', height: '10rem', borderRadius: '20rem', background: "#043c6e", marginTop: '8rem' }}>
+                    <Typography sx={{ color: '#ebe480', fontWeight: 'bold', fontSize: '2rem', 
+                                        ml: '2.5rem', paddingTop: '3.5rem'
+                                    }}
+                    >
+                        Rick
+                    </Typography>
+                </Box>
+                </div>
+
+                <div>
+                <Box sx={{ width: '10rem', height: '10rem', borderRadius: '20rem', background: "#043c6e", 
+                            marginTop: '-2rem'
+                }}>
+                    <Typography sx={{ color: '#ebe480', fontWeight: 'bold', fontSize: '2rem', 
+                                        mt: '3.5rem', ml: '1rem', paddingTop: '3.5rem'
+                                    }}
+                    >
+                        Summer
+                    </Typography>
                 </Box>
 
-                <Box sx={{ mt: '1rem', width: '10rem', height: '10rem', borderRadius: '20rem', background: "#043c6e", }}>
-                    
+                <Box sx={{ width: '10rem', height: '10rem', borderRadius: '20rem', background: "#043c6e", 
+                            marginTop: '4rem'
+                }}>
+                    <Typography sx={{ color: '#ebe480', fontWeight: 'bold', fontSize: '2rem', 
+                                        mt: '3.5rem', ml: '2.5rem', paddingTop: '3.5rem'
+                                    }}
+                    >
+                        Jerry
+                    </Typography>
                 </Box>
+                </div>
 
             </Box>
         </>
